4ddcd5a0d1834efc80539ab743a3d59d43809855
kishor0925 <kishor25092004@gmail.com>
2026-03-27 10:36:12 +0530

    Nodejs

--- FileSystem.js
+++ FileSystem.js
@@ -9,66 +9,64 @@ const fs = require('fs');
 if(fs.existsSync('./data')){
     console.log('Folder is alredy Created')
 }
-else
-{
+else{
 fs.mkdir('./data' , (err) => {
-    console.log(err.message);
-}) 
+    console.log(err);
+})
 }
 
 //Write File 
 
-fs.writeFile('./data/file.txt' , 'Hi Hi Hi Kishor m', (err) => {
-    if(err){
-        console.log(err.message);
-    }
-    else
-    {
-        console.log('File Written')
-    }
-});
+// fs.writeFile('./data/file.txt' , 'Hi Hi Hi Kishor m', (err) => {
+//     if(err){
+//         console.log(err.message);
+//     }
+//     else
+//     {
+//         console.log('File Written')
+//     }
+// });
 
 
 //Read File 
 
 
-if(fs.existsSync('./data/file.txt')){
+// if(fs.existsSync('./data/file.txt')){
 
-    fs.readFile('./data/file.txt', (err, data) => {
-    if(err){
-        console.log(err.message)
-    }
-    else{
-        console.log(data.toString())
-    }
- })
-}
+//     fs.readFile('./data/file.txt', (err, data) => {
+//     if(err){
+//         console.log(err.message)
+//     }
+//     else{
+//         console.log(data.toString())
+//     }
+//  })
+// }
 
 //Delete file 
 
-if(fs.existsSync('./data/file.txt')){
-    fs.unlink('./data/file.txt' , (err) =>{ 
-        if(err)
-        {
-            console.log(err.message);
-        }
-        else
-        {
-            console.log('File Deleted succesfully');
-        }
-    })
-}
+// if(fs.existsSync('./docs/HugeFiles.txt')){
+//     fs.unlink('./docs/HugeFiles.txt' , (err) =>{ 
+//         if(err)
+//         {
+//             console.log(err.message);
+//         }
+//         else
+//         {
+//             console.log('File Deleted succesfully');
+//         }
+//     })
+// }
 
 //Delete folder 
-
-if(fs.existsSync('./data')){
-    fs.rmdir('./data' , (err) => {
-        if(err){
-            console.log(err.message)
-        }
-        else
-        {
-            console.log('Folder Deleted');
-        }
-    })
-}
+// if(fs.existsSync('./docs')){
+//     fs.rmdir('./docs' , (err) => {
+//         if(err){
+//             console.log(err.message)
+//         }
+//         else
+//         {
+//             console.log('Folder Deleted');
+//         }
+//     })
+// }
--- streams.js
+++ streams.js
@@ -1,12 +1,23 @@
 const fs = require('fs');
 
-const readstream = fs.createReadStream('./docs/HugeFile.txt', { encoding: 'utf-8' });
-const writestream = fs.createWriteStream('./docs/HugeFiles.txt');
+
+// fs.writeFile('./data/file.txt' , 'Hi HI HI HI HI IHI HI' , (err) => {
+//     if(err){
+//         console.log(err)
+//     }
+//     else
+//     {
+//         console.log('File Written');
+//     }
+// })
+
+const readstream = fs.createReadStream('./data/file.txt', { encoding: 'utf-8' });
+// const writestream = fs.createWriteStream('./docs/HugeFiles.txt');
 
 readstream.on('data', (chunk) => {
-    writestream.write(chunk);
+    console.log(chunk)
 });
 
-readstream.on('end', () => {
-    console.log('File copied successfully!');
-});
+// readstream.on('end', () => {
+//     console.log('File copied successfully!');
+// });
